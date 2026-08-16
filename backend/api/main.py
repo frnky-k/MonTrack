afcac73db206996b72e77467d2c2335f03f6ec76
unknown <frangkykusuma08@gmail.com>
2026-08-16 16:37:57 +0700

    add frontend vercel link

--- backend/api/main.py
+++ backend/api/main.py
@@ -5,7 +5,7 @@ from api.routes import transactions
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://montrack-v1.vercel.app/"],
+    allow_origins=["http://localhost:5173", "https://montrack-v1.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
